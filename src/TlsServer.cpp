ba8c1e84ac93e251e25af105b6fb5a020d7a79ed
nilshenrich <nilshenrich@web.de>
2022-12-01 11:23:04 +0100

    Added worker function that can be passed to constructor for working on new extablished connections

--- src/TlsServer.cpp
+++ src/TlsServer.cpp
@@ -3,12 +3,14 @@
 using namespace networking;
 using namespace std;
 
-TlsServer::TlsServer(function<void(const int)> workOnClosed,
-                     function<ostream *(int)> os) : NetworkListener{workOnClosed, os} {}
+TlsServer::TlsServer(function<void(const int)> workOnEstablished,
+                     function<void(const int)> workOnClosed,
+                     function<ostream *(int)> os) : NetworkListener{workOnEstablished, workOnClosed, os} {}
 TlsServer::TlsServer(char delimiter,
                      function<void(const int, const string)> workOnMessage,
+                     function<void(const int)> workOnEstablished,
                      function<void(const int)> workOnClosed,
-                     size_t messageMaxLen) : NetworkListener{delimiter, workOnMessage, workOnClosed, messageMaxLen} {}
+                     size_t messageMaxLen) : NetworkListener{delimiter, workOnMessage, workOnEstablished, workOnClosed, messageMaxLen} {}
 
 TlsServer::~TlsServer()
 {
